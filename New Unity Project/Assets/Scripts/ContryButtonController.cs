c7ded97fdec4aa4419a563e5291f7d7241fb216a
Minho Jang <minho9301@naver.com>
2019-11-04 20:56:42 +0900

    List를 통해서 버튼 리스트를 자동 생성

--- New Unity Project/Assets/Scripts/ContryButtonController.cs
+++ New Unity Project/Assets/Scripts/ContryButtonController.cs
@@ -10,35 +10,42 @@ public class ContryButtonController : MonoBehaviour
     void Start()
     {
         // 나라 리스트
-        country = new ArrayList(new string[] { "china", "japan" });
+        country = new ArrayList(new string[] { "china", "japan"  });
 
         foreach (string str in country)
         {
-            // 문자열로 버튼 추출
-            Button btn = GameObject.Find("btn_" + str).GetComponent<Button>();
-
-            // 이름으로 나라모양 버튼 추출
-            CountryButton country = new CountryButton(btn.name.Substring(4));
-
-            // 리스너 등록
-            btn.onClick.AddListener(() => { TaskOnClick(country); }) ;
+            string btn_name = "btn_" + str;
+            GameObject buttonPrefab = GameObject.Find("btn_country");
+            
+            // 리스트마다 버튼 생성
+            CreateButton(str, buttonPrefab, GameObject.Find("Content_btnCountry").transform);
         }
+    }
 
+    public void CreateButton(string name, GameObject prefab, Transform panel)
+    {
+        GameObject button = (GameObject)Instantiate(prefab);
+        CountryButton countrybutton = new CountryButton(name);
+
+        button.transform.SetParent(panel);                                                          // parent 정해서 위치 정하고
+        button.GetComponent<Button>().onClick.AddListener(() => { TaskOnClick(countrybutton); });   // 리스너 추가하고
+        // TODO 매치되는 나라 한글이름 List 필요
+        button.transform.GetChild(0).GetComponent<Text>().text = name;                              // 이름 설정하고 (현재는 영어이름 그대로 씀.
+        button.name = "btn_" + name;                                                                // id 설정하고
     }
 
     void TaskOnClick(CountryButton country)
     {
         if (country.state == "clicked")
         {
-            country.AgainClicked();
+            country.AgainClick();
         } else
         {
             country.BtnOnClick();
         }
     }
 
-    // TODO 모든 것을 public으로 쓰지 않으면 에러가 난다.
-    public class CountryButton
+    class CountryButton
     {
         public SVGImage svg;
         public string name;
@@ -61,7 +68,7 @@ public class ContryButtonController : MonoBehaviour
             state = "clicked";
         }
 
-        public void AgainClicked()
+        public void AgainClick()
         {
             // 지도 색(초록)
             svg.color = new Color32(82, 152, 63, 0);
