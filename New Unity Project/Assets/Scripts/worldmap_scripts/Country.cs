0d20528c5e0f2c48d7034c4799b83c6eb4b1d96e
Minho Jang <minho9301@naver.com>
2019-11-21 21:39:06 +0900

    지도완성!!!!!!!!!!!!!!!!!!!!

---
+++ New Unity Project/Assets/Scripts/worldmap_scripts/Country.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Country
+{
+    string name;
+    Vector2 location;
+
+    public Country(string n, float x, float y)
+    {
+        name = n;
+        location = new Vector2(x, y);
+    }
+
+    public Vector2 getLocation()
+    {
+        return location;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+}
