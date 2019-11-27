d9bfb94476a11892b50283d38629aecfce1e90c2
2JeongYun <47492788+2JeongYun@users.noreply.github.com>
2019-11-27 19:38:54 +0900

    UI 미세변경 - 사운드/버튼
    버튼변경
    버튼사운드추가

---
+++ sogong/Assets/Scripts/chamggae_script/SoundManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    public void playSound(string objectName) {
+        audioSource = GameObject.Find(objectName).GetComponent<AudioSource> ();
+        audioSource.Play();
+    }
+
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
