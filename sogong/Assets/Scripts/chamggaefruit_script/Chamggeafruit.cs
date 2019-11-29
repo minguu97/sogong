ea91b287df0fb87932183bf25b866b3bc7d47d32
hgbgrq <57474913+hgbgrq@users.noreply.github.com>
2019-11-30 01:05:36 +0900

    참깨 과일버전ver1추가

---
+++ sogong/Assets/Scripts/chamggaefruit_script/Chamggeafruit.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+using UnityEngine;
+using UnityEngine.UI;
+public class Chamggeafruit : MonoBehaviour
+{
+    public Image fruit;
+    public Image ending;
+    Sprite[] fruitImages;
+    ArrayList folderNames;
+    int imageIndex = 0;
+    int folderIndex = 0;
+
+    public Button answerImage;
+    public Button nextImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        folderNames = new ArrayList();
+        LoadFolders();
+        Nextfruit();
+        ending.gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LoadFolders()
+    {
+        string Path = "Assets/Resources/fruit";
+
+        DirectoryInfo dict = new DirectoryInfo(Path);
+        foreach (DirectoryInfo directory in dict.GetDirectories())
+        {
+            folderNames.Add(directory.Name);
+            Debug.Log(directory.Name);
+        }
+    }
+
+    private void LoadAnimalImages(string folderName)
+    {
+        string path = "fruit/" + folderName;
+        fruitImages = Resources.LoadAll<Sprite>(path);
+    
+    }
+
+    public void Nextfruit()
+    {
+        if (folderNames.Count == folderIndex)
+        {
+            ending.gameObject.SetActive(true);
+            fruit.gameObject.SetActive(false);
+        }
+        else
+        {
+            LoadAnimalImages((string)folderNames[folderIndex]);
+            fruit.GetComponent<Image>().sprite = fruitImages[imageIndex];
+        }
+    }
+
+    public void onclickanswerImage()
+    {
+        imageIndex++;
+        Nextfruit();
+    }
+
+    public void onclicknextImage()
+    {
+        folderIndex++;
+        imageIndex = 0;
+        Nextfruit();
+    }
+}
