59b6a19a9031116d5cf6aedbff26eab8dd086aae
dewcked <rkdcksdn1234@gmail.com>
2019-11-30 14:19:39 +0900

    resolution

---
+++ sogong/Assets/Scripts/Resolution.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resolution : MonoBehaviour
+{
+    int SetWidth = 1024;
+    int SetHeight = 768;
+    bool fullscreen = true;  // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(Screen.width, Screen.height, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
